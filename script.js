2a54bc0b41ad82f50f58f0a993e9b6aa35740042
Smith Bhojak <99sbhojak@gmail.com>
2022-10-12 18:36:52 +0530

    Uploading Website

---
+++ script.js
@@ -0,0 +1,11 @@
+$.get("../navbar.html", function(data){
+    $(".navbar").replaceWith(data);
+});
+$.get("../search.html", function(data){
+    $(".search").replaceWith(data);
+});
+
+ $.get("../footer.html", function(data){
+   $(".footer").replaceWith(data);
+ });
+
