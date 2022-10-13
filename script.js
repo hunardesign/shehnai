7d02e2f21e21a49291b35e2471a39c0c15be8f90
Smith Bhojak <hunardesignstudios@gmail.com>
2022-10-13 19:19:48 +0530

    Made it responsive for mobile

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $.get("navbar.html", function(data){
     $(".navbar").replaceWith(data);
 });
-$.get("search.html", function(data){
-    $(".search").replaceWith(data);
-});
 
  $.get("footer.html", function(data){
    $(".footer").replaceWith(data);
  });
-
