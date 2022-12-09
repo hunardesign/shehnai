0f16fab864cb6fc741a46e3c07d9df6ef2f63a9d
Smith Bhojak <hunardesignstudios@gmail.com>
2022-12-09 20:04:05 +0530

    meh

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ $.get("navbar.html", function(data){
    $(".footer").replaceWith(data);
  });
 
- 
+ function Activity(){
+  
+  let hide = document.querySelector('.profileSub');
+  hide.style.display = "none";
+  
+  let show = document.querySelector("activitySub");
+  show.style.display = "flex";
+  
+  document.querySelector(".activeTab").setAttribute("class","disableTab")
+  document.querySelector(".disableTab").setAttribute("class","activeTab")
+  
+
+ }
