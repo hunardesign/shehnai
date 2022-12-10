0c9da5658879a12d27bb58928cc8d72484a5588d
Smith Bhojak <hunardesignstudios@gmail.com>
2022-12-10 12:46:36 +0530

    Added View Profile

--- script.js
+++ script.js
@@ -11,11 +11,23 @@ $.get("navbar.html", function(data){
   let hide = document.querySelector('.profileSub');
   hide.style.display = "none";
   
-  let show = document.querySelector("activitySub");
+  let show = document.querySelector(".activitySub");
   show.style.display = "flex";
   
   document.querySelector(".activeTab").setAttribute("class","disableTab")
   document.querySelector(".disableTab").setAttribute("class","activeTab")
   
 
+ }
+
+ function MyProfile(){
+  let hide = document.querySelector('.activitySub');
+  hide.style.display = "none";
+  
+  let show = document.querySelector(".profileSub");
+  show.style.display = "flex";
+  
+  document.querySelector(".disableTab").setAttribute("class","activeTab")
+  document.querySelector(".activeTab").setAttribute("class","disableTab")
+  
  }
