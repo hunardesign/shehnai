9ec2dd001068ba3268a8327cce5bde757b1fb313
Smith Bhojak <hunardesignstudios@gmail.com>
2022-12-10 18:16:11 +0530

    View Profile Works

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ $.get("navbar.html", function(data){
 
  function Activity(){
   
-  let hide = document.querySelector('.profileSub');
-  hide.style.display = "none";
+  let none = document.querySelector('.profileSub');
+  none.style.display = "none";
   
   let show = document.querySelector(".activitySub");
   show.style.display = "flex";
@@ -21,8 +21,8 @@ $.get("navbar.html", function(data){
  }
 
  function MyProfile(){
-  let hide = document.querySelector('.activitySub');
-  hide.style.display = "none";
+  let none = document.querySelector('.activitySub');
+  none.style.display = "none";
   
   let show = document.querySelector(".profileSub");
   show.style.display = "flex";
@@ -30,4 +30,62 @@ $.get("navbar.html", function(data){
   document.querySelector(".disableTab").setAttribute("class","activeTab")
   document.querySelector(".activeTab").setAttribute("class","disableTab")
   
+ }
+
+ function viewAbout(){
+  document.querySelector('#activeProfileNav').setAttribute('id','');
+  document.querySelector('.viewAbout').setAttribute('id','activeProfileNav')
+  
+  document.querySelector('.aboutGrid').style.display='grid'
+  document.querySelector('.familyDetails').style.display='none'
+  document.querySelector('.partnerPreferences').style.display='none'
+  document.querySelector('.otherDetails').style.display='none'
+  document.querySelector('.contactDetails').style.display='none'
+
+
+
+ }
+
+ function viewFamilyDetails(){
+  document.querySelector('#activeProfileNav').setAttribute('id','');
+  document.querySelector('.viewFamilyDetails').setAttribute('id','activeProfileNav')
+
+  document.querySelector('.partnerPreferences').style.display='none'
+  document.querySelector('.aboutGrid').style.display='none'
+  document.querySelector('.otherDetails').style.display='none'
+  document.querySelector('.contactDetails').style.display='none'
+  document.querySelector('.familyDetails').style.display='flex'
+  
+ }
+ function viewPartnerPreferences(){
+  document.querySelector('#activeProfileNav').setAttribute('id','');
+  document.querySelector('.viewPartnerPreferences').setAttribute('id','activeProfileNav')
+
+  document.querySelector('.familyDetails').style.display='none'
+  document.querySelector('.aboutGrid').style.display='none'
+  document.querySelector('.otherDetails').style.display='none'
+  document.querySelector('.contactDetails').style.display='none'
+  document.querySelector('.partnerPreferences').style.display='flex'
+
+ }
+ function viewOtherDetails(){
+  document.querySelector('#activeProfileNav').setAttribute('id','');
+  document.querySelector('.viewOtherDetails').setAttribute('id','activeProfileNav')
+
+  document.querySelector('.familyDetails').style.display='none'
+  document.querySelector('.aboutGrid').style.display='none'
+  document.querySelector('.partnerPreferences').style.display='none'
+  document.querySelector('.contactDetails').style.display='none'
+  document.querySelector('.otherDetails').style.display='flex'
+ }
+ function viewContactDetails(){
+  document.querySelector('#activeProfileNav').setAttribute('id','');
+  document.querySelector('.viewContactDetails').setAttribute('id','activeProfileNav')
+
+  document.querySelector('.familyDetails').style.display='none'
+  document.querySelector('.aboutGrid').style.display='none'
+  document.querySelector('.partnerPreferences').style.display='none'
+  document.querySelector('.otherDetails').style.display='none'
+  document.querySelector('.contactDetails').style.display='flex'
+
  }
